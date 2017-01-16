4de504163969e421df50afee25740459a0251faa
Raymond Neilson <rneilson@interloper.ca>
2017-01-15 23:35:01 -0500

    Replaced db.open() code with Sqlite class

--- src/main.js
+++ src/main.js
@@ -1,53 +1,107 @@
 /**
- * SQLite client library for Node.js applications
+ * Pooled SQLite client library for Node.js
  *
- * Copyright © 2016 Kriasoft, LLC. All rights reserved.
+ * Copyright © 2017 Raymond Neilson. All rights reserved.
+ *
+ * Some code copyright © 2016 Kriasoft, LLC. All rights reserved.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
  */
 
 import sqlite3 from 'sqlite3';
+import genericPool from 'generic-pool';
 import Database from './Database';
+import Transaction from './Transaction';
 
-const promise = global.Promise;
-const db = new Database(null, { Promise: promise });
+// Default options
+const defaults = {
+  // sqlite defaults
+  mode: null,
+  busyTimeout: 1000,
+  foreignKeys: true,
+  walMode: true,
 
-/**
- * Opens SQLite database.
- *
- * @returns Promise<Database> A promise that resolves to an instance of SQLite database client.
- */
-db.open = (filename, { mode = null, verbose = false, Promise = promise } = {}) => {
-  let driver;
+  // pool defaults
+  min: 1,
+  max: 4,
+  acquireTimeout: 1000,
 
-  if (verbose) {
-    sqlite3.verbose();
-  }
+  // internal defaults
+  trxImmediate: true,
 
-  return new Promise((resolve, reject) => {
-    if (mode !== null) {
-      driver = new sqlite3.Database(filename, mode, (err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    } else {
-      driver = new sqlite3.Database(filename, (err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    }
-  }).then(() => {
-    db.driver = driver;
-    db.Promise = Promise;
-    return new Database(driver, { Promise });
-  });
+  // general defaults
+  Promise: global.Promise
 };
 
-export default db;
+class Sqlite {
+  constructor (filename, options) {
+    // Extract options
+    const {
+      mode,
+      busyTimeout,
+      foreignKeys,
+      walMode,
+      min,
+      max,
+      trxImmediate,
+      acquireTimeout,
+      Promise
+    } = Object.assign({}, defaults, options);
+
+    // Re-consolidate options
+    this.pool_options = { min, max, Promise, acquireTimeoutMillis: acquireTimeout };
+    this.sqlite_options = { mode, busyTimeout, foreignKeys, walMode };
+    this.trxImmediate = trxImmediate;
+    this.Promise = Promise;
+
+    // Factory functions for generic-pool
+    this.pool_factory = {
+      create: async () => {
+        // Create database connection, wait until open complete
+        let connection = await new Promise((resolve, reject) => {
+          let driver;
+
+          if (mode !== null) {
+            driver = new sqlite3.Database(filename, mode, callback);
+          }
+          else {
+            driver = new sqlite3.Database(filename, callback);
+          }
+
+          // Busy timeout default hardcoded to 1000ms, so
+          // only configure if a different value given
+          if (busyTimeout !== 1000) {
+            driver.configure('busyTimeout', busyTimeout);
+          }
+
+          function callback (err) {
+            if (err) {
+              return reject(err);
+            }
+            return resolve(new Database(driver, { Promise }));
+          }
+        });
+
+        // Set foreign keys and/or WAL mode as appropriate
+        if (foreignKeys) {
+          await connection.exec('PRAGMA foreign_keys = ON;');
+        }
+        if (walMode) {
+          await connection.exec('PRAGMA journal_mode = WAL;');
+        }
+
+        // Return now-configured db connection
+        return connection;
+      },
+
+      destroy: (connection) => connection.close()
+    };
+
+    // Create pool
+    this.pool = genericPool.createPool(this.pool_factory, this.pool_options);
+  }
+
+}
+
+export default Sqlite;
