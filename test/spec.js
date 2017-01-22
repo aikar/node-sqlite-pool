50bb7874ef6bce759818169d382eeba8ac5e33f0
Raymond Neilson <rneilson@interloper.ca>
2017-01-22 00:32:49 -0500

    Updated tests for new package layout/hierarchy

--- test/spec.js
+++ test/spec.js
@@ -7,11 +7,12 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-const db = require('../build/main');
+const Sqlite = require('../build/main');
 const expect = require('chai').expect;
 
 it('Should open a database connection', (done) => {
-  let p = db.open(':memory:');
+  const db = new Sqlite(':memory:');
+  let p = Promise.resolve();
   p = p.then(() => db.exec('CREATE TABLE tbl (col TEXT)'));
   p = p.then(() => db.exec('INSERT INTO tbl VALUES ("test")'));
   p = p.then(() => db.get('SELECT col FROM tbl').then((result) => {
@@ -37,7 +38,8 @@ it('Should open a database connection', (done) => {
 });
 
 it('Should allow named parameters to be used', (done) => {
-  let p = db.open(':memory:');
+  const db = new Sqlite(':memory:');
+  let p = Promise.resolve();
   p = p.then(() => db.exec('CREATE TABLE tbl (col TEXT)'));
   p = p.then(() => db.run('INSERT INTO tbl(col) VALUES (:col)', { ':col': 'something' })
     .then((stmt) => {
@@ -66,74 +68,83 @@ it('Should allow named parameters to be used', (done) => {
 });
 
 it('Should allow named parameters to be used with prepared statements', (done) => {
-  let p = db.open(':memory:');
-  p = p.then(() => db.exec('CREATE TABLE tbl (col TEXT)'));
-  p = p.then(() => db.prepare('INSERT INTO tbl(col) VALUES (:col)', { ':col': 'some text' })
-    .then(stmt => stmt.run())
-    .then((stmt) => {
-      expect(stmt.lastID).to.equal(1);
-      return stmt.finalize();
-    }));
-  p = p.then(() => db.prepare('SELECT col FROM tbl WHERE 1 = ? AND 5 = ?5')
-    .then(stmt => stmt.bind({ 1: 1, 5: 5 }))
-    .then(stmt => stmt.get()
-      .then((result) => {
-        expect(result).to.be.deep.equal({ col: 'some text' });
+  const db = new Sqlite(':memory:');
+  let p = db.use((conn) => {
+    let q = Promise.resolve();
+    q = q.then(() => conn.exec('CREATE TABLE tbl (col TEXT)'));
+    q = q.then(() => conn.prepare('INSERT INTO tbl(col) VALUES (:col)', { ':col': 'some text' })
+      .then(stmt => stmt.run())
+      .then((stmt) => {
+        expect(stmt.lastID).to.equal(1);
+        return stmt.finalize();
+      }));
+    q = q.then(() => conn.prepare('SELECT col FROM tbl WHERE 1 = ? AND 5 = ?5')
+      .then(stmt => stmt.bind({ 1: 1, 5: 5 }))
+      .then(stmt => stmt.get()
+        .then((result) => {
+          expect(result).to.be.deep.equal({ col: 'some text' });
+          return stmt.finalize();
+        })));
+    q = q.then(() => conn.prepare('INSERT INTO tbl(col) VALUES ($col)')
+      .then(stmt => stmt.run({ $col: 'other text' }))
+      .then((stmt) => {
+        expect(stmt.lastID).to.equal(2);
+        return stmt.finalize();
+      }));
+    q = q.then(() => conn.prepare('SELECT col FROM tbl WHERE ROWID = ?')
+      .then(stmt => stmt.each([2], (err, result) => {
+        expect(result).to.be.deep.equal({ col: 'other text' });
+      }).then((rowsCount) => {
+        expect(rowsCount).to.equal(1);
         return stmt.finalize();
       })));
-  p = p.then(() => db.prepare('INSERT INTO tbl(col) VALUES ($col)')
-    .then(stmt => stmt.run({ $col: 'other text' }))
-    .then((stmt) => {
-      expect(stmt.lastID).to.equal(2);
-      return stmt.finalize();
-    }));
-  p = p.then(() => db.prepare('SELECT col FROM tbl WHERE ROWID = ?')
-    .then(stmt => stmt.each([2], (err, result) => {
-      expect(result).to.be.deep.equal({ col: 'other text' });
-    }).then((rowsCount) => {
-      expect(rowsCount).to.equal(1);
-      return stmt.finalize();
-    })));
-  p = p.then(() => db.prepare('SELECT col FROM tbl WHERE 13 = @thirteen ORDER BY col DESC')
-    .then(stmt => stmt.all({ '@thirteen': 13 })
-      .then((results) => {
-        expect(results).to.be.deep.equal([{ col: 'some text' }, { col: 'other text' }]);
-        return stmt.finalize();
-      })));
+    q = q.then(() => conn.prepare('SELECT col FROM tbl WHERE 13 = @thirteen ORDER BY col DESC')
+      .then(stmt => stmt.all({ '@thirteen': 13 })
+        .then((results) => {
+          expect(results).to.be.deep.equal([{ col: 'some text' }, { col: 'other text' }]);
+          return stmt.finalize();
+        })));
+    return q;
+  });
   p = p.then(() => db.close());
   p.then(done, done);
 });
 
 it('Should allow chaining Statement.run() calls', (done) => {
-  let p = db.open(':memory:');
-  p = p.then(() => db.exec('CREATE TABLE tbl (col1 TEXT, col2 TEXT, col3 TEXT)'));
-  p = p.then(() => db.prepare('INSERT INTO tbl(col1, col2, col3) VALUES (?, ?, ?)')
-    .then(stmt => stmt.run('a1', 'a2', 'a3')).then((stmt) => {
-      expect(stmt.lastID).to.equal(1);
-      return stmt.run('b1', 'b2', 'b3');
-    })
-    .then((stmt) => {
-      expect(stmt.lastID).to.equal(2);
-      return stmt.finalize();
+  const db = new Sqlite(':memory:');
+  let p = db.use((conn) => {
+    let q = Promise.resolve();
+    q = q.then(() => conn.exec('CREATE TABLE tbl (col1 TEXT, col2 TEXT, col3 TEXT)'));
+    q = q.then(() => conn.prepare('INSERT INTO tbl(col1, col2, col3) VALUES (?, ?, ?)')
+      .then(stmt => stmt.run('a1', 'a2', 'a3')).then((stmt) => {
+        expect(stmt.lastID).to.equal(1);
+        return stmt.run('b1', 'b2', 'b3');
+      })
+      .then((stmt) => {
+        expect(stmt.lastID).to.equal(2);
+        return stmt.finalize();
+      }));
+    q = q.then(() => conn.all('SELECT col1, col2, col3 FROM tbl').then((results) => {
+      expect(results).to.be.deep.equal([{
+        col1: 'a1',
+        col2: 'a2',
+        col3: 'a3',
+      }, {
+        col1: 'b1',
+        col2: 'b2',
+        col3: 'b3',
+      }]);
     }));
-  p = p.then(() => db.all('SELECT col1, col2, col3 FROM tbl').then((results) => {
-    expect(results).to.be.deep.equal([{
-      col1: 'a1',
-      col2: 'a2',
-      col3: 'a3',
-    }, {
-      col1: 'b1',
-      col2: 'b2',
-      col3: 'b3',
-    }]);
-  }));
+    return q;
+  });
   p = p.then(() => db.close());
   p.then(done, done);
 });
 
 it('Should handle BLOBs', (done) => {
   const buf = Buffer.from('SGVsbG8gd29ybGQh', 'base64');
-  let p = db.open(':memory:');
+  const db = new Sqlite(':memory:');
+  let p = Promise.resolve();
   p = p.then(() => db.exec('CREATE TABLE dat (b BLOB)'));
   p = p.then(() => db.run('INSERT INTO dat(b) VALUES(?)', buf).then((stmt) => {
     expect(stmt.lastID).to.equal(1);
@@ -146,7 +157,8 @@ it('Should handle BLOBs', (done) => {
 });
 
 it('Should migrate the database', (done) => {
-  let p = db.open(':memory:');
+  const db = new Sqlite(':memory:');
+  let p = Promise.resolve();
   p = p.then(() => db.migrate());
   p = p.then(() => db.all('SELECT id, name FROM migrations').then((result) => {
     expect(result).to.be.deep.equal([{ id: 1, name: 'initial' }, { id: 2, name: 'some-feature' }]);
